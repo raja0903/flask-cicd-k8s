81c47d47884de20f8e4e5685acfa40fe79765c1f
aman <amantrivedi.knp@gmail.com>
2025-11-15 12:03:19 +0100

    Added Prometheus metrics endpoint

--- app.py
+++ app.py
@@ -1,10 +1,19 @@
 from flask import Flask
+from prometheus_client import Counter, generate_latest
+
 app = Flask(__name__)
+
+REQUESTS = Counter("flask_requests_total", "Total HTTP requests")
 
 @app.route("/")
 def hello():
+    REQUESTS.inc()
     return "🚀 Flask CI/CD on Kubernetes Works!"
 
+@app.route("/metrics")
+def metrics():
+    return generate_latest(), 200, {"Content-Type": "text/plain"}
+    
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
 
